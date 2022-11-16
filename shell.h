b0699f433fb82d7f0b118bbf9248d66c2abc099e
edowaye <edowayeadenaiya@gmail.com>
2022-11-16 05:13:47 -0800

    Updated shell.h file

--- shell.h
+++ shell.h
@@ -89,4 +89,10 @@ int _strcmp(char *, char *);
 char *starts_with(const char *, const char *);
 char *_strcat(char *, char *);
 
+/*_string1.c */
+char *_strcpy(char *, char *);
+char *_strdup(const char *);
+void _puts(char *);
+int _putchar(char);
+
 #endif
